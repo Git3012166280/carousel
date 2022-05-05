0e29487acff6278723c7f5aeb1966d0dcfe1210e
YCJ <15047150695@163.com>
2022-05-05 19:49:54 +0800

    优化布局

--- app/src/main/java/com/rotationmap/example/MainActivity.java
+++ app/src/main/java/com/rotationmap/example/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mList = new ArrayList<>();
         for (int i : mImages) {
             mList.add(i);
